e644898b1ee2ddc8d64f48e6e9a35c318fb1fa2a
Gladson-Samuel-S <samuelgladson4@gmail.com>
2021-10-19 14:48:33 +0530

    Initial Commit :fire:

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6beb1777714fa80fe9077c0631823a17'),
+    },
+  },
+});
