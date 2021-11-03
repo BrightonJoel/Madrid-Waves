e665a924bf4503371547acb1693ebeaf2b54568b
BrightonJoel <sbjoel112000@gmail.com>
2021-11-03 22:52:25 +0530

    Create page without file hadling

--- frontend/src/pages/BlogPages/CreateBlog.js
+++ frontend/src/pages/BlogPages/CreateBlog.js
@@ -1,48 +1,145 @@
-import React from "react"
-import { MainDiv, CreateForm } from "./CreateBlogStyles"
-import { Button } from "../../styles/GlobalComponents/Button"
-import { FaPlusSquare, FaTimes } from "react-icons/fa"
+import React, { useRef } from "react";
+import { MainDiv, CreateForm } from "./CreateBlogStyles";
+import { Button } from "../../styles/GlobalComponents/Button";
+import { FaPlusSquare, FaTimes } from "react-icons/fa";
+import useUser from "../../hooks/useUser";
+import { useHistory } from "react-router";
+import { useMutation, gql, useQuery } from "@apollo/client";
+import { ErrorWrapper } from "../AuthPages/AuthPageStyles";
+
+const CREATEBLOG = gql`
+  mutation CreateBlog(
+    $Title: String!
+    $Body: String!
+    $Author: ID
+    $blogCategories: [ID]
+  ) {
+    createBlog(
+      input: {
+        data: {
+          Title: $Title
+          Body: $Body
+          Author: $Author
+          blogCategories: $blogCategories
+        }
+      }
+    ) {
+      blog {
+        Title
+      }
+    }
+  }
+`;
+
+const FETCHCATEGORY = gql`
+  query fetchCategory {
+    blogCategories {
+      id
+      Name
+    }
+  }
+`;
 
 export default function CreateBlog() {
-  return (
-    <MainDiv>
-      <h3>Create A New Blog</h3>
-      <CreateForm>
-        <form>
-          <label>Title</label>
-          <input type='text'></input>
-          <label>Category</label>
-          <select>
-            <option value='volvo'>Volvo</option>
-            <option value='saab'>Saab</option>
-            <option value='mercedes'>Mercedes</option>
-            <option value='audi'>Audi</option>
-          </select>
-          <label> Body </label>
-          <textarea rows='25'></textarea>
-          <label> Upload Image</label>
-          <input type='file' accept='image/*'></input>
-          <Button
-            bg={({ theme }) => theme.colors.yellow}
-            clr={({ theme }) => theme.colors.primaryBlue}
-          >
-            Create
-            <span>
-              <FaPlusSquare />
-            </span>
-          </Button>
-          <Button
-            bg={({ theme }) => theme.colors.yellow}
-            clr={({ theme }) => theme.colors.primaryBlue}
-            action='clear'
-          >
-            Clear
-            <span>
-              <FaTimes />
-            </span>
-          </Button>
-        </form>
-      </CreateForm>
-    </MainDiv>
-  )
+  const title = useRef();
+  const category = useRef();
+  const body = useRef();
+  const image = useRef();
+  const history = useHistory();
+  const { currentUser, loading } = useUser();
+
+  const [
+    createBlog,
+    {
+      data: createBlogData,
+      loading: createBlogLoading,
+      error: createBlogError,
+    },
+  ] = useMutation(CREATEBLOG, {
+    options: {
+      context: {
+        headers: {
+          credentials: "include",
+        },
+      },
+    },
+  });
+
+  const { data } = useQuery(FETCHCATEGORY);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(title.current.value);
+    console.log(category.current.value);
+    console.log(body.current.value);
+    console.log(image.current.value);
+
+    createBlog({
+      variables: {
+        Title: title.current.value,
+        Body: body.current.value,
+        Author: currentUser.id,
+        blogCategories: category.current.value,
+      },
+    });
+  }
+
+  if (loading) {
+    return <p> loading pls wait</p>;
+  } else if (currentUser) {
+    return (
+      <MainDiv>
+        <h3>Create A New Blog</h3>
+        <CreateForm>
+          <form onSubmit={handleSubmit}>
+            <label>Title</label>
+            <input type="text" ref={title} required></input>
+            <label>Category</label>
+            <select ref={category} required>
+              {data.blogCategories.map((category) => (
+                <option key={category.id} value={category.id}>
+                  {category.Name}
+                </option>
+              ))}
+            </select>
+            <label> Body </label>
+            <textarea rows="25" ref={body} required></textarea>
+            <label> Upload Image</label>
+            <input type="file" accept="image/*" ref={image}></input>
+            {!createBlogLoading && !createBlogError && createBlogData ? (
+              <ErrorWrapper>Blog Created Successfully !!!</ErrorWrapper>
+            ) : !createBlogData && !createBlogError ? (
+              ""
+            ) : (
+              <ErrorWrapper>An error has occured !</ErrorWrapper>
+            )}
+
+            <Button
+              bg={({ theme }) => theme.colors.yellow}
+              clr={({ theme }) => theme.colors.primaryBlue}
+              type="submit"
+            >
+              {createBlogLoading ? "Creating" : "Create"}
+              <span>
+                <FaPlusSquare />
+              </span>
+            </Button>
+            <Button
+              bg={({ theme }) => theme.colors.yellow}
+              clr={({ theme }) => theme.colors.primaryBlue}
+              action="clear"
+            >
+              Clear
+              <span>
+                <FaTimes />
+              </span>
+            </Button>
+          </form>
+        </CreateForm>
+      </MainDiv>
+    );
+  } else {
+    history.push("/login");
+    return null;
+  }
 }
